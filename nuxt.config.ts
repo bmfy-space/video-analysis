382603433f384a5bb5d719cf32415713fa34d332
bb0103 <bb0103@foxmail.com>
2024-06-06 19:25:48 +0800

    update cloudflare deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-
+  buildDir: '.nuxt',
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    }
+  },
   modules: ["@nuxt/ui"]
 })
