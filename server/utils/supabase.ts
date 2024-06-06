ca58986ea7d11f0ae9ca287ac3d9e453635b0c8b
bb0103 <bb0103@foxmail.com>
2024-06-06 19:05:55 +0800

    update supabase

--- server/utils/supabase.ts
+++ server/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl: string = process.env.NUXT_PUBLIC_SUPABASE_URL as string
-const supabaseKey: string = process.env.NUXT_PUBLIC_SUPABASE_KEY as string
+const supabaseUrl: string = process.env.NUXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL as string
+const supabaseKey: string = process.env.NUXT_PUBLIC_SUPABASE_KEY || process.env.SUPABASE_KEY as string
 
 export default createClient(supabaseUrl, supabaseKey)
